7cea33a8a5c8532c54a55b397e7d9b86ad6c63f6
Franco Nicolini <franconicolini111@gmail.com>
2022-05-16 16:25:25 -0300

    Delete TALLER/tp1_taller directory

--- TALLER/tp1_taller/target/debug/tp1_taller.d
+++
@@ -1,1 +0,0 @@
-/home/franco/Escritorio/TALLER/tp1_taller/target/debug/tp1_taller: /home/franco/Escritorio/TALLER/tp1_taller/src/grid.rs /home/franco/Escritorio/TALLER/tp1_taller/src/main.rs
--- TALLER/tp1_taller/target/doc/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["tp1_taller"] = {"name":"","files":["grid.rs","main.rs"]};
-createSourceSidebar();
