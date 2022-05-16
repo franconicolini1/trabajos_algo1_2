2261a04c4bb9dbb35d006178f97b009c3e542007
Franco Nicolini <franconicolini111@gmail.com>
2022-05-16 16:26:16 -0300

    Delete practicando_react directory

--- practicando_react/login_firebase/src/components/Admin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-    return (
-        <div>Esto es admin</div>
-    )
-}
-
-export default Login
--- practicando_react/login_firebase/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-    return (
-        <div>Esto es Home</div>
-    )
-}
-
-export default Home
